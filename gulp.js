4a34d6355e922d74300bc90dfb7ed5264bb853a9
Robert Gabriel <robert_gabriel@outlook.com>
2015-09-03 00:23:37 +0100

    Removed css to less and added gulp

---
+++ gulp.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var jshint = require('gulp-jshint');
+var gutil = require('gulp-util');
+var minifyCSS = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+
+
+gulp.task('less', function () {
+    gulp.src('/assests/css/styles.less')
+        .pipe(less()
+            .on('error', gutil.log)
+            .on('error', gutil.beep)
+            .on('error', function (err) {
+                console.log('err', err);
+                var pathToFile = err.fileName.split('\\');
+                file = pathToFile[pathToFile.length - 1];
+                // say.speak('Albert', 'Less is fucked---' + file + '--- Line ' + err.lineNumber);
+            })
+        )
+        .pipe(minifyCSS({
+            keepSpecialComments: 1
+        }))
+        .pipe(gulp.dest('/assests/public/css/'));
+});
+
+
+gulp.task('compressJs', function () {
+    return gulp.src('/assests/js/site.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('/assests/public/'));
+});
+
+gulp.task('build', ['less', 'compressJs']);
+
+gulp.task('default', ['build']);
