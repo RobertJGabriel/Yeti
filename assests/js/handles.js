33070bef27e6a79704bba88dc798262c68026dac
Robert Gabriel <robert_gabriel@outlook.com>
2015-09-30 10:39:21 +0100

    Added Search Engine

--- assests/js/handles.js
+++ assests/js/handles.js
@@ -1,246 +1,183 @@
-$(function () {
-
-    var myParam = location.search.split('apikey=')[1];
-    var term = location.search.split('term=')[1];
-    if(myParam !== ''){
-            $("#webresults > *").remove();
-      ajaxGetRequest( "/yeti/v1/getsearches.json?apikey=" + myParam + "&term=" + term +"");
+$(function() {
+    var myParam = getParameterByName('apikey');
+    var term = getParameterByName('term');
+    if (myParam !== '') {
+        $("#webresults > *").remove();
+        ajaxGetRequest("/yeti/v1/getsearches.json?apikey=" + myParam + "&term=" + term + "");
     }
-
-
-    $("#search_bar").submit(function () {
-        alert('s');
-            var myParam = location.search.split('apikey=')[1];
-        window.location = "http://localhost/yeti/search?apikey=" + myParam + "&term=k";
-       
+    $("#search_bar").submit(function() {
+        var myParam = getParameterByName('apikey');
+        var term = getParameterByName('term');
+        var searchTerm = document.getElementById('search_bar_input').value;
+        window.location = "http://localhost/yeti/search?apikey=" + myParam + "&term=" + searchTerm + "";
+        return false; // avoid to execute the actual submit of the form.
     });
-
-    $("#deleteButton").click(function () {
+    $("#deleteButton").click(function() {
         alert('ss');
-        ajaxPostRequest("", "/yeti/v1/deleteaccount","");
- 
+        ajaxPostRequest("", "/yeti/v1/deleteaccount", "");
         return false; // avoid to execute the actual submit of the form.
     });
-
-
-  
-    $("#signin").submit(function () {
-       ajaxPostRequest($(this), "/yeti/v1/signin","");
+    $("#signin").submit(function() {
+        ajaxPostRequest($(this), "/yeti/v1/signin", "");
         return false; // avoid to execute the actual submit of the form.
     });
-
-    $("#signup").submit(function () {
-        ajaxPostRequest($(this), "/yeti/v1/signup","");
+    $("#signup").submit(function() {
+        ajaxPostRequest($(this), "/yeti/v1/signup", "");
         return false; // avoid to execute the actual submit of the form.
     });
-
-      // This is for the personal Settings
-    $("#delete_account").submit(function () {
-        ajaxPostRequest($(this), "/yeti/v1/deleteaccount",""); // the script where you handle the form input.
-        return false; // avoid to execute the actual submit of the form.
-    });
-
     // This is for the personal Settings
-    $("#update_account").submit(function () {
-        ajaxPostRequest($(this), "/yeti/v1/updateaccount","");
+    $("#delete_account").submit(function() {
+        ajaxPostRequest($(this), "/yeti/v1/deleteaccount", ""); // the script where you handle the form input.
         return false; // avoid to execute the actual submit of the form.
     });
-
-  // This is for the personal Settings
-    $("#new_search_settings").submit(function () {
+    // This is for the personal Settings
+    $("#update_account").submit(function() {
+        ajaxPostRequest($(this), "/yeti/v1/updateaccount", "");
+        return false; // avoid to execute the actual submit of the form.
+    });
+    // This is for the personal Settings
+    $("#new_search_settings").submit(function() {
         alert('batman');
-        ajaxPostRequest($(this), "/yeti/v1/updateSearchSettings","");
+        ajaxPostRequest($(this), "/yeti/v1/updateSearchSettings", "");
         return false; // avoid to execute the actual submit of the form.
     });
-
-
-     // This is for the personal Settings
-    $("#manualImportSearch").submit(function () {
-        alert('batman is cool and this worked ?');
-        ajaxPostRequest($(this), "/yeti/v1/manualImportSearch","");
-        return false; // avoid to execute the actual submit of the form.
-    });
-
     // This is for the personal Settings
-    $("#manualImportEmployee").submit(function () {
-        alert('batman is cool and this worked, employyee ?');
-        ajaxPostRequest($(this), "/yeti/v1/manualImportEmployee","");
+    $("#manualImportSearch").submit(function() {
+        alert('batman is cool and this worked ?');
+        ajaxPostRequest($(this), "/yeti/v1/manualImportSearch", "");
         return false; // avoid to execute the actual submit of the form.
     });
-
-
+    // This is for the personal Settings
+    $("#manualImportEmployee").submit(function() {
+        alert('batman is cool and this worked, employyee ?');
+        ajaxPostRequest($(this), "/yeti/v1/manualImportEmployee", "");
+        return false; // avoid to execute the actual submit of the form.
+    });
     // Used for checking the url 
     function regexUrlextensioncheck(n) {
-    {
-        var s = document.URL,
-            e = new RegExp(n);
-        e.test(s)
-    }
-    return e.test(s)
-    }
-
-    function getTable(){
-
-        
+        {
+            var s = document.URL,
+                e = new RegExp(n);
+            e.test(s)
+        }
+        return e.test(s)
     }
 
-
-
-
+    function getTable() {}
     var el = document.getElementById("plusInput");
-        el.addEventListener("click", createInput);
+    el.addEventListener("click", createInput);
 
-
-    function createInput(){
-
+    function createInput() {
         var div = document.createElement("div");
-            div.setAttribute("class", "form-group");
-
+        div.setAttribute("class", "form-group");
         var input = document.createElement("input");
-            input.setAttribute("type", "text");
-            input.setAttribute("class", "form-control floating-label");
-            input.setAttribute("name", "information[]");
-            input.setAttribute("placeholder", "Something Extra");
-
-          
+        input.setAttribute("type", "text");
+        input.setAttribute("class", "form-control floating-label");
+        input.setAttribute("name", "information[]");
+        input.setAttribute("placeholder", "Something Extra");
         div.appendChild(input);
-
         document.getElementById("addedInput").appendChild(div);
-
     }
 
-
-
-
-    function ajaxPostRequest(thisObj,url,message){
+    function ajaxPostRequest(thisObj, url, message) {
         console.log(thisObj);
-          $.ajax({
+        $.ajax({
             type: "POST",
             cache: false,
             url: url,
             data: thisObj.serialize(), // serializes the form's elements.
-            success: function (data) {
+            success: function(data) {
                 console.log(data);
-              
-                   alerts(data,message); //Handles log in
-                
+                alerts(data, message); //Handles log in
             }
         });
     }
 
-    function ajaxGetRequest(url,message){
+    function ajaxGetRequest(url, message) {
         $.ajax({
             url: url,
             type: "GET",
             dataType: "json",
-            success: function (data) {
+            success: function(data) {
                 console.log('aa' + data);
-                jQuery.each(data, function (i, val) {
+                jQuery.each(data, function(i, val) {
                     createSearchResult(val);
                 });
             }
         });
     }
 
-    
-
     function appendSearchResult(val) {
         var ul = document.getElementById("popluarSearches");
         var li = document.createElement("li");
         li.appendChild(document.createTextNode(val['description']));
         ul.appendChild(li);
-
-
-
-
-
-
-
-
     }
 
-
-    function createSearchResult(val){
-    
-
-    var test  = document.getElementById('webresults');
-
-    var div = document.createElement("div");
+    function createSearchResult(val) {
+        var test = document.getElementById('webresults');
+        var div = document.createElement("div");
         div.setAttribute("class", "panel panel-success");
         div.setAttribute("role", "alert");
         div.setAttribute("id", "");
-    
-    var a = document.createElement('a');
-    var linkText = document.createTextNode(val['title']);
-a.appendChild(linkText);
-a.title = val['title'];
-a.href = val['url'];
-
-    var div2 = document.createElement("div");
+        var a = document.createElement('a');
+        var linkText = document.createTextNode(val['title']);
+        a.appendChild(linkText);
+        a.title = val['title'];
+        a.href = val['url'];
+        var div2 = document.createElement("div");
         div2.setAttribute("class", "panel-heading");
-
         div.appendChild(div2);
-
-    var div3 = document.createElement("h3");
+        var div3 = document.createElement("h3");
         div3.setAttribute("class", "panel-title");
         div3.appendChild(a);
-
         div2.appendChild(div3);
-
-    var div4 = document.createElement("div");
+        var div4 = document.createElement("div");
         div4.setAttribute("class", "panel-body");
         div4.innerHTML = val['description'];
         div.appendChild(div4);
-
-    test.appendChild(div);
-
-
-
+        test.appendChild(div);
     }
 
     function alerts(status, message) {
-  
         $("#alert > *").remove();
-        
         switch (status) {
-        case "true":
-            createAlertDiv("Awesome, Hold on two seconds", "success");
-            setTimeout(redirect, 2000);
-            break;
-        case "error":
-            createAlertDiv("Oh snap something is wrong ", "danger");
-            break;
-        case "passwordchanged":
-            createAlertDiv("password Changed", "success");
-            break;
+            case "true":
+                createAlertDiv("Awesome, Hold on two seconds", "success");
+                setTimeout(redirect, 2000);
+                break;
+            case "error":
+                createAlertDiv("Oh snap something is wrong ", "danger");
+                break;
+            case "passwordchanged":
+                createAlertDiv("password Changed", "success");
+                break;
         }
     }
 
+    function getParameterByName(name) {
+        name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+            results = regex.exec(location.search);
+        return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
 
-
-
-
-    function createAlertDiv(message,type){
-        
+    function createAlertDiv(message, type) {
         var div = document.createElement("div");
-            div.setAttribute("class", "alert alert-dismissable alert-" + type);
-            div.setAttribute("role", "alert");
-            div.setAttribute("id", "alertDiv");
-            div.innerHTML = message;
-        
+        div.setAttribute("class", "alert alert-dismissable alert-" + type);
+        div.setAttribute("role", "alert");
+        div.setAttribute("id", "alertDiv");
+        div.innerHTML = message;
         var button = document.createElement("button");
-            button.setAttribute("type", "button");
-            button.setAttribute("class", "close");
-            button.setAttribute("data-dismiss", "alert");
-            button.innerHTML = "x";
-            div.appendChild(button);
-
+        button.setAttribute("type", "button");
+        button.setAttribute("class", "close");
+        button.setAttribute("data-dismiss", "alert");
+        button.innerHTML = "x";
+        div.appendChild(button);
         document.getElementById("alerts").appendChild(div);
     }
 
     function redirect() {
         window.location = "";
     }
-
-
 });
